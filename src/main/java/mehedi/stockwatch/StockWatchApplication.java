bc23b3b17e436496073aff1ce9f46c5c48d93aeb
Mehedihasan2026 <mahadicoc2016@gmail.com>
2026-09-09 20:51:16 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/mehedi/stockwatch/StockWatchApplication.java
@@ -0,0 +1,13 @@
+package mehedi.stockwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mehedi/stockwatch/StockWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package mehedi.stockwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
