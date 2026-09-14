1198f5d3272c7eba0ec3f8f6742b7aa1f3f0728c
Mehedihasan2026 <mahadicoc2016@gmail.com>
2026-09-14 19:50:56 +0200

    Added Service "Initial"

---
+++ src/main/java/mehedi/stockwatch/service/StockService.java
@@ -0,0 +1,63 @@
+package mehedi.stockwatch.service;
+
+import mehedi.stockwatch.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import mehedi.stockwatch.dto.CreateStockRequest;
+import mehedi.stockwatch.dto.StockResponse;
+import mehedi.stockwatch.entity.Stock;
+
+import java.time.LocalDateTime;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+    public StockResponse createStock(CreateStockRequest request) {
+
+        Stock stock = new Stock();
+
+        stock.setTicker(request.ticker());
+        stock.setCompanyName(request.companyName());
+        stock.setShares(request.shares());
+        stock.setBuyPrice(request.buyPrice());
+        stock.setCurrency(request.currency());
+        stock.setTargetPrice(request.targetPrice());
+        stock.setNotes(request.notes());
+
+        stock.setAlertEnabled(
+                request.alertEnabled() != null
+                        ? request.alertEnabled()
+                        : true
+        );
+
+        stock.setAlertTriggered(false);
+
+        LocalDateTime now = LocalDateTime.now();
+        stock.setCreatedAt(now);
+        stock.setUpdatedAt(now);
+
+        Stock savedStock = stockRepository.save(stock);
+
+        return toResponse(savedStock);
+    }
+    private StockResponse toResponse(Stock stock) {
+        return new StockResponse(
+                stock.getId(),
+                stock.getTicker(),
+                stock.getCompanyName(),
+                stock.getShares(),
+                stock.getBuyPrice(),
+                stock.getCurrency(),
+                stock.getTargetPrice(),
+                stock.getNotes(),
+                stock.getAlertEnabled(),
+                stock.getAlertTriggered(),
+                stock.getCreatedAt(),
+                stock.getUpdatedAt()
+        );
+    }
+}
